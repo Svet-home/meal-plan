aba1779c97273b39e68e40f87b7bbe15761803f6
Svet-home <aprelkovakatish@gmail.com>
2023-03-27 09:00:01 +0300

    meal two

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Meal Plan</h1>
+      <input type="text" placeholder="ADD a meal"/>
+      <button>ADD</button>
+    </div>
+  );
+}
+
+export default App;
